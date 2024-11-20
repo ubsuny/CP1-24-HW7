8459ea5888a9432f25f9a3cefe29373706e62121
HeshamElsaman <88336539+HeshamElsaman@users.noreply.github.com>
2024-11-20 07:59:03 +0000

    Added the function to find roots using the tangent method with scipy and its test

--- calculus.py
+++ calculus.py
@@ -1,8 +1,27 @@
 """
 calculus.py
 """
+from scipy import optimize
+
 
 def dummy():
     """ dummy functions for template file
     just the same function that Dr Thomay made"""
     return 0
+
+# Function that uses the tangent method for root-finding
+def root_tangent(function, fprime, x0):
+    """
+    A function that takes a function, its derivative, and an initial guess
+    to estimate the root closest to that initial guess
+
+    Parameters:
+    Inputs:
+    function (function): a function that defines a mathematically specific functional form
+    fprime (function): a function that defines the mathematically functional form of the
+                        function's derivative
+    x0: an initial guess that's as close as possible to one of the roots
+    Outputs:
+    (number): the desired root (zero) of the function
+    """
+    return optimize.newton(function, x0, fprime)
--- test_calculus.py
+++ test_calculus.py
@@ -1,9 +1,55 @@
 """
 test_calculus.py
 """
+import pytest
+import numpy as np
 import calculus as calc
+
+def func_1(x):
+    """
+    A simple function to be used for testing
+
+    Parameters:
+    Inputs:
+    x (number): the independent variable
+    Outputs:
+    (number): the value of the function
+    """
+    return x ** 2
+
+def func_1_prime(x):
+    """
+    A simple function to be used for testing
+
+    Parameters:
+    Inputs:
+    x (number): the independent variable
+    Outputs:
+    (number): the value of the derivative of func_1
+    """
+    return 2 * x
+
+@pytest.fixture
+def func_1_x_0():
+    """
+    A function that returns a value for x as a first guess to be used for testing
+    """
+    return 0.0073
 
 def test_dummy():
     """ Unit test for dummy function
     just the same test function that Dr Thomay made"""
     assert calc.dummy() == 0
+
+def test_root_tangent(func_1_x_0):
+    """
+    A function that tests the wrapper implementation for tangent method for root-finding
+
+    Parameters:
+    Inputs:
+    func_1 (function): the function to find its root
+    func_1_prime (function): the derivative of the function
+    x_0 (number): the initial guess for the root
+    """
+    compare = calc.root_tangent(func_1, func_1_prime, func_1_x_0)
+    assert np.isclose(compare, 0.0, atol = 1.0e-6)
