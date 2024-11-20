6b05e398b55ac8f1830503428cc19e2130a42531
ojha-aditya <adityaojha.science@gmail.com>
2024-11-20 11:49:25 +0000

    Implemented the Trapezoidal method using numpy and scipy based wrapper function and made the code functional to plot the integral.

--- calculus.py
+++ calculus.py
@@ -1,8 +1,67 @@
 """
-calculus.py
+This module implements different integration and root finding algorithms
 """
 
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
 def dummy():
-    """ dummy functions for template file
-    just the same function that Dr Thomay made"""
+    """ 
+    dummy function for template file
+    """
     return 0
+
+def trapezoid_numpy(func, l_lim, u_lim, steps=10000):
+    '''
+    Function to implement trapezoidal rule using numpy and plot the result
+    '''
+    x = np.linspace(l_lim, u_lim, steps+1)  # create a linear grid between upper and lower limit
+    y = func(x) # evaluate the function on the grid
+    integral_value = np.trapezoid(y, x) # calculate the integral using numpy
+
+    integral_function = np.zeros(len(x))    # a zero array for integral at each point
+
+    for i in range(1, len(x)):
+        # calculate the integral at each x for plotting
+        integral_function[i] = np.trapezoid(y[:i+1], x[:i+1])
+
+    #dx = np.diff(x)
+    #integral_function = np.cumsum(dx * (y[:-1] + y[1:]) / 2)
+
+    # plotting the original and integrated functions
+    plt.plot(x, y, label = '$f(x)$')
+    # plt.plot(x[:-1], integral_function, label = '$\\int_a^b f(x) dx$')
+    plt.plot(x, integral_function, label = '$\\int f(t) dt$: area under $y = f(x)$')
+    plt.ylabel('y(x)')
+    plt.xlabel('x')
+    plt.legend()
+
+    return integral_value
+
+def trapezoid_scipy(f, a, b, n=10000):
+    '''
+    Function to implement trapezoidal rule using scipy and plot the result
+    '''
+    x = np.linspace(a, b, n+1)  # create a linear grid between upper and lower limit
+    y = f(x)    # evaluate the function on the grid
+    integral_value = sp.integrate.trapezoid(y, x)   # calculate the integral using numpy
+
+    integral_function = np.zeros(len(x))    # a zero array for integral at each point
+
+    for i in range(1, len(x)):
+        # calculate the integral at each x for plotting
+        integral_function[i] = sp.integrate.trapezoid(y[:i+1], x[:i+1])
+
+    # dx = np.diff(x)
+    # integral_function = np.cumsum(dx * (y[:-1] + y[1:]) / 2)
+
+    # plotting the original and integrated functions
+    plt.plot(x, y, label = '$f(x)$')
+    # plt.plot(x[:-1], integral_function, label = '$\\int_a^b f(x) dx$')
+    plt.plot(x, integral_function, label = '$\\int f(t) dt$: area under $y = f(x)$')
+    plt.ylabel('y(x)')
+    plt.xlabel('x')
+    plt.legend()
+
+    return integral_value
--- test_calculus.py
+++ test_calculus.py
@@ -1,9 +1,22 @@
 """
 test_calculus.py
 """
+import numpy as np
 import calculus as calc
 
 def test_dummy():
     """ Unit test for dummy function
     just the same test function that Dr Thomay made"""
     assert calc.dummy() == 0
+
+def test_trapezoid_numpy():
+    '''
+    Unit test for numpy implementation of trapezoid method
+    '''
+    assert np.isclose(calc.trapezoid_numpy(np.sin, 0, np.pi), 1.9999999835506594)
+
+def test_trapezoid_scipy():
+    '''
+    Unit test for numpy implementation of trapezoid method
+    '''
+    assert np.isclose(calc.trapezoid_scipy(np.sin, 0, np.pi), 1.9999999835506594)
