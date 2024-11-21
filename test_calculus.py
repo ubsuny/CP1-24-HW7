4558d40e1331d10b858f3e844450282be7311857
Priyanka_43 <PRIYANK3@BUFFALO.EDU>
2024-11-21 11:02:26 -0500

    Fixed pytest error. remove assert code line and write new one.

--- test_calculus.py
+++ test_calculus.py
@@ -3,7 +3,7 @@ test_calculus.py
 """
 
 import numpy as np
-
+import calculus as calc
 from calculus import scipy_simpson
 # Define the function to integrate outside the test function
 
@@ -11,16 +11,5 @@ def test_scipy_simpson():
     """
     test the scipy implementation for simpson method
     """
-    def test_func(x):
-        return x**3 + 1  # Function to integrate
-    # Define the range for integration
-    a, b = -1, 1
-
-    # Expected result (integral of x^3 + 1 from -1 to 1 is 2)
-    expected_result = 2.0
-    result = scipy_simpson(test_func, a, b)
-    print(f"Result: {result}")
-
     # Use an assertion to check if the result is close to the expected value
-    assert np.isclose(result, expected_result, atol=1e-6), \
-        f"Expected {expected_result}, but got {result}"
+    assert np.isclose(calc.scipy_simpson(x**3 + 1, -1, 1), 2)
