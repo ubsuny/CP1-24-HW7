13ca804f4d27118c5e43c06d8f7baba6c0eb69d9
ojha-aditya <adityaojha.science@gmail.com>
2024-11-21 01:19:09 +0000

    Removed the plotting functionality from trapezoid implementation and added a safeguard for infinite values of function at any limit or x value. Improved the docstrings to contained detailed function description. Rectified small typos in unittest modules.

--- calculus.py
+++ calculus.py
@@ -4,7 +4,7 @@ This module implements different integration and root finding algorithms
 
 import numpy as np
 import scipy as sp
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 def dummy():
     """ 
@@ -14,11 +14,22 @@ def dummy():
 
 def trapezoid_numpy(func, l_lim, u_lim, steps=10000):
     '''
-    Function to implement trapezoidal rule using numpy and plot the result
-    '''
-    x = np.linspace(l_lim, u_lim, steps+1)  # create a linear grid between upper and lower limit
-    y = func(x) # evaluate the function on the grid
-    integral_value = np.trapezoid(y, x) # calculate the integral using numpy
+    This function implements trapezoidal rule using numpy wrapper function
+    by evaluating the integral of the input function over the limits given
+    in the input number of steps and gives as output the integral value in numpy 
+    floating point decimal. If the input function is infinite at any point that point
+    is modified to a slightly higher value.
+
+    Parameters:
+    - func: integrand (could be a custom defined function or a standard function like np.sin)
+    - l_lim: lower limit of integration
+    - u_lim: upper limit of integration
+    - steps: number of steps (default value 10000)
+
+    Returns:
+    - integral of the input function using numpy.trapezoid function
+
+    Once the integral value is calculated, it can be compared graphically as follows:
 
     integral_function = np.zeros(len(x))    # a zero array for integral at each point
 
@@ -36,18 +47,53 @@ def trapezoid_numpy(func, l_lim, u_lim, steps=10000):
     plt.xlabel('x')
     plt.title('integral using numpy trapezoidal method; steps = '+str(steps))
     plt.legend()
+    '''
 
+    # check if the integrand is infinite at lower limit, if yes slightly change the limit
+    try:
+        func(l_lim)
+    except ZeroDivisionError:
+        l_lim += 0.000000001
+
+    # check if the integrand is infinite at upper limit, if yes slightly change the limit
+    try:
+        func(u_lim)
+    except ZeroDivisionError:
+        u_lim += 0.000000001
+
+    x = np.linspace(l_lim, u_lim, steps+1)  # create a linear grid between upper and lower limit
+
+    for i, xi in enumerate((x)):
+        # check if the integrand is infinite at any x, if yes slightly change the x value
+        try:
+            func(xi)
+        except ZeroDivisionError:
+            x[i] += 0.000000001
+
+    y = func(x) # evaluate the function on the modified grid
+    integral_value = np.trapezoid(y, x) # calculate the integral using numpy
     return integral_value
 
 def trapezoid_scipy(func, l_lim, u_lim, steps=10000):
     '''
-    Function to implement trapezoidal rule using scipy and plot the result
-    '''
-    x = np.linspace(l_lim, u_lim, steps+1)  # create a linear grid between upper and lower limit
-    y = func(x)    # evaluate the function on the grid
-    integral_value = sp.integrate.trapezoid(y, x)   # calculate the integral using numpy
+    This function implements trapezoidal rule using scipy wrapper function
+    by evaluating the integral of the input function over the limits given
+    in the input number of steps and gives as output the integral value in numpy 
+    floating point decimal. If the input function is infinite at any point that point
+    is modified to a slightly higher value.
 
-    integral_function = np.zeros(len(x))    # a zero array for integral at each point
+    Parameters:
+    - func: integrand(could be a custom defined function or a standard function like np.sin)
+    - l_lim: lower limit of integration
+    - u_lim: upper limit of integration
+    - steps: number of steps (default value 10000)
+
+    Returns:
+    - integral of the input function using scipy.integrate.trapezoid function
+
+    Once the integral value is calculated, it can be compared graphically as follows:
+
+       integral_function = np.zeros(len(x))    # a zero array for integral at each point
 
     for i in range(1, len(x)):
         # calculate the integral at each x for plotting
@@ -63,5 +109,29 @@ def trapezoid_scipy(func, l_lim, u_lim, steps=10000):
     plt.ylabel('y(x)')
     plt.xlabel('x')
     plt.legend()
+    '''
 
+    # check if the integrand is infinite at lower limit, if yes slightly change the limit
+    try:
+        func(l_lim)
+    except ZeroDivisionError:
+        l_lim += 0.000000001
+
+    # check if the integrand is infinite at upper limit, if yes slightly change the limit
+    try:
+        func(u_lim)
+    except ZeroDivisionError:
+        u_lim += 0.000000001
+
+    x = np.linspace(l_lim, u_lim, steps+1)  # create a linear grid between upper and lower limit
+
+    for i, xi in enumerate((x)):
+        # check if the integrand is infinite at any x, if yes slightly change the x value
+        try:
+            func(xi)
+        except ZeroDivisionError:
+            x[i] += 0.000000001
+
+    y = func(x)    # evaluate the function on the modified grid
+    integral_value = sp.integrate.trapezoid(y, x)   # calculate the integral using numpy
     return integral_value
--- test_calculus.py
+++ test_calculus.py
@@ -5,18 +5,19 @@ import numpy as np
 import calculus as calc
 
 def test_dummy():
-    """ Unit test for dummy function
-    just the same test function that Dr Thomay made"""
+    """ 
+    Unit test for dummy function
+    """
     assert calc.dummy() == 0
 
 def test_trapezoid_numpy():
     '''
     Unit test for numpy implementation of trapezoid method
     '''
-    assert np.isclose(calc.trapezoid_numpy(np.sin, 0, np.pi), 1.9999999835506594)
+    assert np.isclose(calc.trapezoid_numpy(np.sin, 0, np.pi), 2)
 
 def test_trapezoid_scipy():
     '''
-    Unit test for numpy implementation of trapezoid method
+    Unit test for scipy implementation of trapezoid method
     '''
-    assert np.isclose(calc.trapezoid_scipy(np.sin, 0, np.pi), 1.9999999835506594)
+    assert np.isclose(calc.trapezoid_scipy(np.sin, 0, np.pi), 2)
