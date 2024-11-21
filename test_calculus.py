9f252ab776e3428d7a49148af433d4d0eef307a3
Priyanka_43 <PRIYANK3@BUFFALO.EDU>
2024-11-21 00:49:56 -0500

    Fixed syntax error,

--- test_calculus.py
+++ test_calculus.py
@@ -12,7 +12,7 @@ def test_scipy_simpson():
     test the scipy implementation for simpson method
     """
     def test_func(x):
-    return x**3 + 1  # Function to integrate
+        return x**3 + 1  # Function to integrate
     
     # Expected result (integral of x^3 + 1 from -1 to 1 is 2)
     expected_result = 2.0
