51cc1e0e2e713be20479d383d7905bee1be387b8
abruns123 <aabruns123@gmail.com>
2024-11-22 19:15:33 +0000

    adjusted for general cases

--- calculus.py
+++ calculus.py
@@ -18,57 +18,15 @@ def a_trap(y, d):
     """
     return np.trapezoid(y,dx=d)
 
-def func3(x):
-    """
-    func3 acts as the 
-    x^3+1 function
-    """
-    return x**3+1
-
-def d3(x):
-    """
-    d3 is the derivative of 
-    function func3
-    """
-    return 3*x**2
-
-def func1(x):
-    """
-    func1 acts as the 
-    exp(-1/x) function
-    """
-    return np.exp(-1/x)
-
-def d1(x):
-    """
-    d1 is the derivative of 
-    function func1
-    """
-    return -1/x*np.exp(-1/x)
-
-def func2(x):
-    """
-    func2 acts as the 
-    cos(1/x) function
-    """
-    return np.cos(1/x)
-
-def d2(x):
-    """
-    d2 is the derivative of 
-    function func2
-    """
-    return -1/x*np.sin(1/x)
+def sec_derivative(func, x,dx):
+    return np.gradient(np.gradient(func(x)),dx)
 
 def adapt(func, bounds, d, sens):
     """
     adapt uses adaptive trapezoidal integration
     to integrate a function over boundaries.
-    func must be a str defining the function
-    to be integrated. May be:
-    x^3+1
-    exp(-1/x)
-    cos(1/x)
+    func must be function which outputs a list
+    of its values.
     bounds is a list of length two which defines
     the lower and upper bounds of integration
     d defines the number of points between
@@ -78,37 +36,26 @@ def adapt(func, bounds, d, sens):
     how the function changes. 
     """
     #The x is defined as a linspace which is used to define
-    #the derivative of the function for each point x
+    #the second derivative of the function for each point x
     #between the bounds
     x=np.linspace(bounds[0], bounds[1], d+1)
     dx=x[1]-x[0]
-    if func=="x^3+1":
-        dydx=d3(x)
-    elif func=="exp(-1/x)":
-        dydx=d1(x)
-    elif func=="cos(1/x)":
-        dydx=d2(x)
+    d2ydx2=sec_derivative(func,x,dx)
 
-    
-    iter=enumerate(x)
-    sum=0
-    #a loop is run through x. Each derivative is used to 
-    #define the number of indices of new_x, which is a 
+    loopx=enumerate(x)
+    summer=0
+    #a loop is run through x. Each second derivative is used to
+    #define the number of indices of new_x, which is a
     #list defining a number of points inbetween 2 x values
     #then, trapezoidal integration is conducted over new_x
     #and each integration is summed with eachother to produce
     #the total integral.
-    for count, val in iter:
+    for count, val in loopx:
         if count!=len(x)-1:
-            new_x=np.linspace(val, x[count+1], 2*(int(np.abs(sens*dydx[count]))+1))
-            if func=="x^3+1":
-                new_y=func3(new_x)
-            elif func=="exp(-1/x)":
-                new_y=func1(new_x)
-            elif func=="cos(1/x)":
-                new_y=func2(new_x)
-            sum+=a_trap(new_y, dx/((2*(int(np.abs(sens*dydx[count]))+1))-1))
-    return sum
+            new_x=np.linspace(val, x[count+1], 2*(int(np.abs(sens*d2ydx2[count]))+1))
+            new_y=func(new_x)
+            summer+=a_trap(new_y, dx/((2*(int(np.abs(sens*d2ydx2[count]))+1))-1))
+    return summer
 
 
 
