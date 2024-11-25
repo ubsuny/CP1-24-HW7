6218bd0d0efd1e8717b477b745547aab9f93a7f3
HeshamElsaman <88336539+HeshamElsaman@users.noreply.github.com>
2024-11-25 23:48:09 +0000

    Removed some redundancy

--- calculus.py
+++ calculus.py
@@ -6,7 +6,6 @@ import os
 import ctypes
 import math
 import numpy as np
-from scipy import optimize
 import scipy as sp
 from scipy.integrate import simpson
 
@@ -90,7 +89,7 @@ def root_tangent(function, fprime, x0, tolerance = 1e-6, maxiterations = 1000):
     Outputs:
     (number): the desired root (zero) of the function
     """
-    return optimize.newton(function, x0, fprime, tol = tolerance, maxiter = maxiterations)
+    return sp.optimize.newton(function, x0, fprime, tol = tolerance, maxiter = maxiterations)
 
 def tangent_pure_python(func, fprime, x0, tol=1e-6, maxiter=50):
     """
