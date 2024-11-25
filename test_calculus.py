f668f4047a7009bde5f6c34c6cda6393d1e90164
abruns123 <aabruns123@gmail.com>
2024-11-25 04:29:48 -0500

    Update test_calculus.py
    added pytest for c++ implementation of adaptive trapezoid to test_calculus

--- test_calculus.py
+++ test_calculus.py
@@ -19,6 +19,40 @@ calculus.verify_arguments.restype = ctypes.c_bool
 calculus.calculate_square.argtypes = [ctypes.c_double]
 calculus.calculate_square.restype = ctypes.c_double
 
+lib =ctypes.cdll.LoadLibrary("./calculus.dll")
+lib.adapt_c.argtypes=[ctypes.CFUNCTYPE(ctypes.c_double,ctypes.c_double), ctypes.c_double, ctypes.c_double, ctypes.c_int, ctypes.c_int]
+lib.adapt_c.restype=ctypes.c_double
+
+def cubic(x):
+    return x**3+1
+
+def cosine(x):
+    return np.cos(1/x)
+
+def exponential(x):
+    return np.exp(-1/x)
+    
+cubic_c=ctypes.CFUNCTYPE(ctypes.c_double, ctypes.c_double)
+python_cubic_c=cubic_c(cubic)
+
+cos_c=ctypes.CFUNCTYPE(ctypes.c_double, ctypes.c_double)
+python_cos_c=cos_c(cosine)
+
+exp_c=ctypes.CFUNCTYPE(ctypes.c_double, ctypes.c_double)
+python_exp_c=exp_c(exponential)
+
+def test_adapt():
+    """
+    test adapt confirms that the adapt_c function 
+    produces the expected results for certain 
+    integrals.
+    """
+    result1=lib.adapt_c(python_cubic_c, -1,1,10,10)
+    assert np.isclose(result1, 2, .1)
+    result2=lib.adapt_c(python_cos_c, .01,3*np.pi, 20,100)
+    assert np.isclose(result2, 7.9, .1)
+    result3=lib.adapt_c(python_exp_c, .01, 10, 100, 1000)
+    assert np.isclose(result3, 7.2, .1)
 # Define the function to integrate outside the test function
 
 def test_wrapper_simpson():
